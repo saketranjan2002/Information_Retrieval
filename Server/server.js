222e7b6e8f7f59cb8d1f7177431758609a08df08
Saket Ranjan <saketranjan2002@gmail.com>
2022-12-03 12:31:02 +0530

    ranking implimented

--- Server/server.js
+++ Server/server.js
@@ -238,13 +238,34 @@ app.post('/api/search/go', async (req, res) => {
         console.log("Results !!")
         console.log(searchResults.length)
         console.log(searchResults);
+
+        function merge_Arrays(left_sub_array, right_sub_array) {
+            let array = []
+            while (left_sub_array.length && right_sub_array.length) {
+               if (left_sub_array.rank > right_sub_array.rank) {
+                  array.push(left_sub_array.shift())
+               } else {
+                  array.push(right_sub_array.shift())
+               }
+            }
+            return [ ...array, ...left_sub_array, ...right_sub_array ]
+        }
+        function merge_sort(unsorted_Array) {
+            const middle_index = unsorted_Array.length / 2
+            if(unsorted_Array.length < 2) {
+                return unsorted_Array
+            }
+            const left_sub_array = unsorted_Array.splice(0, middle_index)
+            return merge_Arrays(merge_sort(left_sub_array),merge_sort(unsorted_Array))
+        }
+        
     
         return res
             .status(200)
             .send({
                 success: true,
                 data: {
-                    docs: searchResults
+                    docs: merge_sort(searchResults)
                 }
             })
     })
