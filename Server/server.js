df37bb111b52025c82917108c000f7c978e1da7e
Saket Ranjan <saketranjan2002@gmail.com>
2022-12-05 18:11:19 +0530

    modified server.js

--- Server/server.js
+++ Server/server.js
@@ -236,25 +236,25 @@ app.post('/api/search/go', async (req, res) => {
     // })
 
 
-    function merge_Arrays(left_sub_array, right_sub_array) {
-        let array = []
-        while (left_sub_array.length && right_sub_array.length) {
-           if (left_sub_array.rank > right_sub_array.rank) {
-              array.push(left_sub_array.shift())
-           } else {
-              array.push(right_sub_array.shift())
-           }
+     function merge_Arrays(left_sub_array, right_sub_array) {
+            let array = []
+            while (left_sub_array.length && right_sub_array.length) {
+               if (left_sub_array.rank > right_sub_array.rank) {
+                  array.push(left_sub_array.shift())
+               } else {
+                  array.push(right_sub_array.shift())
+               }
+            }
+            return [ ...array, ...left_sub_array, ...right_sub_array ]
         }
-        return [ ...array, ...left_sub_array, ...right_sub_array ]
-    }
-    function merge_sort(unsorted_Array) {
-        const middle_index = unsorted_Array.length / 2
-        if(unsorted_Array.length < 2) {
-            return unsorted_Array
+        function merge_sort(unsorted_Array) {
+            const middle_index = unsorted_Array.length / 2
+            if(unsorted_Array.length < 2) {
+                return unsorted_Array
+            }
+            const left_sub_array = unsorted_Array.splice(0, middle_index)
+            return merge_Arrays(merge_sort(left_sub_array),merge_sort(unsorted_Array))
         }
-        const left_sub_array = unsorted_Array.splice(0, middle_index)
-        return merge_Arrays(merge_sort(left_sub_array),merge_sort(unsorted_Array))
-    }
 
     // searchPromise.the
     searchPromise.then(() => {
